06fd3c7aaad187b8e6d6db8c89d7b1abbcb251c8
Karim Tayie <karimsherif2597@gmail.com>
2019-10-02 00:05:17 +0200

    Added project basic setup

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+    next();
+});
+
+fs.readFileSync(path.join(__dirname, 'models')).forEach((filename) => {
+    require(`./models/${filename}`);
+});
+
+app.listen(3000, () => {
+    console.log('Server is listening on port 3000');
+});
